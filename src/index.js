556835f4a0f8165de801c2c5081441ed961dfa4e
abdulhamiid <adioabdulhamid@yahoo.com>
2022-06-03 16:36:22 +0100

    import all modules fuctions from addRemove.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,9 @@
 import './style.css';
+import { getDataFromStorage } from '../modules/addRemove';
+import { addItem } from '../modules/addRemove';
+import { handleClick } from '../modules/addRemove';
 
-const todoList = [
-  {
-    description: 'Wash the dishes',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Wash the car',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Completed todo list project',
-    completed: false,
-    index: 3,
-  },
-];
 
-const list = document.querySelector('#todo-list');
-
-const addItem = () => {
-  todoList.forEach((item) => {
-    list.innerHTML += `
-    <li>
-    <div>
-    <input type="checkbox">
-    <p>${item.description}</p>
-    </div>
-    <i class="bi bi-three-dots-vertical"></i>
-    </li>`;
-  });
-};
-
+getDataFromStorage();
 addItem();
+handleClick();
