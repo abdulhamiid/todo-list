4cf36507580a7e058010b06d8408343dcd11d034
abdulhamiid <adioabdulhamid@yahoo.com>
2022-06-03 16:50:48 +0100

    Add function to ignore empty input

--- modules/addRemove.js
+++ modules/addRemove.js
@@ -28,25 +28,28 @@ export const getDataFromStorage = () => {
 
 export const addItem = (e) => {
   e.preventDefault();
-  const todo = document.querySelector('#todo').value;
-  const todoObj = {
-    description: todo,
-    completed: false,
-    index: todoList.length + 1
-  };
-  document.querySelector('#todo').value = '';
-  todoList.push(todoObj);
-  localStorage.setItem('data', JSON.stringify(todoList));
-    let item = todoList[todoList.length -1]
-    list.innerHTML += `
-    <li class="list">
-    <div>
-    <input type="checkbox">
-    <p contenteditable="true" class="todo-item" id="${item.index}">${item.description}</p>
-    </div>
-    <i class="bi bi-trash3 hide dot-${item.index}" id="${item.index}"></i>
-    <i class="bi bi-three-dots-vertical trash-${item.index}"></i>
-    </li>`;
+  const todo = document.querySelector('#todo');
+  if(todo.value !== ''){
+    const todoObj = {
+      description: todo.value,
+      completed: false,
+      index: todoList.length + 1
+    };
+
+    todo.value = '';
+    todoList.push(todoObj);
+    localStorage.setItem('data', JSON.stringify(todoList));
+      let item = todoList[todoList.length -1]
+      list.innerHTML += `
+      <li class="list">
+      <div>
+      <input type="checkbox">
+      <p contenteditable="true" class="todo-item" id="${item.index}">${item.description}</p>
+      </div>
+      <i class="bi bi-trash3 hide dot-${item.index}" id="${item.index}"></i>
+      <i class="bi bi-three-dots-vertical trash-${item.index}"></i>
+      </li>`;  
+  } 
 };
 
 export const handleClick = (e) => {
