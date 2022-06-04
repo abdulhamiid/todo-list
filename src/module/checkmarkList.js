b10830c91016baf14af01a47925c0283fcc033f4
abdulhamiid <adioabdulhamid@yahoo.com>
2022-06-04 12:45:11 +0100

    Fix linters errors

--- src/module/checkmarkList.js
+++ src/module/checkmarkList.js
@@ -1,17 +1,18 @@
-import { todoList } from "./addRemove";
+import { todoList } from './addRemove.js';
+
 const checkboxes = document.querySelectorAll('input[name="task"]');
-const clear = document.querySelector('#clear');
 
-export const setItem = (e) => {
-  let id = e.target.id;
-  let p = document.querySelector(`.item-${id}`);
+const setItem = (e) => {
+  const { id } = e.target;
+  const p = document.querySelector(`.item-${id}`);
   p.classList.toggle('line');
-  if(!todoList[id - 1].completed){
+  if (!todoList[id - 1].completed) {
     todoList[id - 1].completed = true;
-  }else {
-    todoList[id - 1].completed = false
-; }
+  } else {
+    todoList[id - 1].completed = false;
+  }
   localStorage.setItem('data', JSON.stringify(todoList));
-}
+};
 
-checkboxes.forEach(item => {item.addEventListener('change', setItem)})
+checkboxes.forEach((item) => { item.addEventListener('change', setItem); });
+export default setItem;
