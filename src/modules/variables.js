9f3abcb67a9b7627b3df9c7b5e97130725ba5d69
abdulhamiid <adioabdulhamid@yahoo.com>
2022-06-05 15:41:51 +0100

    Declear all needed variables

---
+++ src/modules/variables.js
@@ -0,0 +1,8 @@
+const list = document.querySelector('#todo-list');
+const form = document.querySelector('form');
+const todo = document.querySelector('#todo');
+const clear = document.querySelector('#clear');
+
+export {
+  list, form, todo, clear,
+};
