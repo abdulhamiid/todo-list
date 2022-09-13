1d09487aad0a95db2056c54d8d156c6c03651850
abdulhamiid <adioabdulhamid@yahoo.com>
2022-09-13 17:28:09 +0300

    Add additional class_name to li

--- src/modules/code.js
+++ src/modules/code.js
@@ -4,7 +4,7 @@ let todoList = [];
 
 const createHtml = (container, obj) => {
   container.innerHTML += `
-  <li class="list">
+  <li class="list flex">
   <div>
   <input type="checkbox" name="task" class="check check-${obj.index}" id="${obj.index}">
   <p contenteditable="true" class="todo-item item-${obj.index}" id="${obj.index}">${obj.description}</p>
